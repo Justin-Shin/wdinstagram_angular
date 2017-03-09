cfaa7f7b5547c6306b27a92b207c2a9a631c75c8
Justin Shin <jjs3b@virginia.edu>
2017-03-09 00:58:17 -0500

    done a little

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+  angular
+    .module("wdinstagram", ["ui.router","ngResource"])
+    .config(["$stateProvider", RouterFunction])
+    .factory("PostFactory",[ "$resource" , PostFactoryFunction])
+    .controller("PostIndexController", ["PostFactory",PostIndexControllerFunction])
+    .controller("PostNewController", ["PostFactory","$state",PostNewControllerFunction])
+    .controller("PostShowController", ["PostFactory","$stateParams",PostShowControllerFunction])
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("wdinstagramIndex", {
+      url: "/wdinstagram",
+      templateUrl: "js/ng-views/index.html",
+      controller: "PostIndexController",
+      controllerAs: "vm"
+    })
+    .state("wdinstagramNew", {
+      url: "/wdinstagram/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "GrumbleNewController",
+      controllerAs: "vm"
+    })
+    .state("wdinstagramShow", {
+      url: "/wdinstagram/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "GrumbleShowController",
+      controllerAs: "vm"
+    });
+  }
+  function PostFactoryFunction($resource){
+    return $resource("http://localhost:3000/grumbles/:id", {}, {
+      update: {method: "PUT"}
+    })
+  }
+  function PostIndexControllerFunction(PostFactory){
+    this.posts = PostFactory.query()
+  }
+  function PostNewControllerFunction(PostFactory,$state){
+
+  }
+  function PostShowControllerFunction(PostFactory,$stateParams){
+
+  }
+
 })();
